aea0b4a41c02fb3a2466364a45b1241d69f5f492
Maggs King <kbmgold@gmail.com>
2015-07-09 18:26:16 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+/*var app = angular.module('app',["firebase"]);
+
+app.controller('myCtrl', ['$scope', 'firebaseObject' function ($scope, $firebaseObject) {
+
+	var ref = new Firebase ("https://chat-exercise3.firebaseio.com/");
+
+		$scope.data = $firebaseObject(ref);
+		$scope.message = 'BON JOVI';
+	}]):
+
+
+
+
+
+
+}]
+
+
+};
